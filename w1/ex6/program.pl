c90f348cd84582155a3d7ab3b31c19c0ce7475fb
Yosvel Reyes <yosvel.reyes@globant.com>
2016-01-04 16:05:55 -0300

    add program input choices

--- w1/ex6/program.pl
+++ w1/ex6/program.pl
@@ -6,44 +6,74 @@ use Circle;
 use Rectangle;
 use Square;
 use Triangle;
+use Data::Dumper;
 
-#working on the circle figure...
-my $circleCenter = Point::->new('x' => 0, 'y' => 0);
-my $circlePoint = Point::->new('x' => 50, 'y' => 50);
+my $command = shift @ARGV;
+my @points;
+my $f;
+my $created = 0;
 
-my $c = Circle::->new(('center' => $circleCenter, 'point' => $circlePoint));
+print Dumper $command;
 
-$c->draw();
-$c->area();
+#parse the points...
+while(my $arg = shift @ARGV){
+    my ($x, $y) = split(",", $arg);
 
-#working on a rectangle now...
-my $r1p = Point::->new('x' => 60, 'y' => 60);
-my $r2p = Point::->new('x' => 320, 'y' => 60);
-my $r3p = Point::->new('x' => 320, 'y' => 120);
-my $r4p = Point::->new('x' => 60, 'y' => 120);
+    if($x > 300 || $y > 300){
+        die "allowed grid is 300x300 so x and y both must be lower than 300";
+    }
 
-my $rect = Rectangle::->new(('p1' => $r1p, 'p2' => $r2p, 'p3' => $r3p, 'p4' => $r4p));
+    my $point = Point::->new('x' => $x, 'y' => $y);
+    $points[scalar @points] = $point;
 
-$rect->draw();
-$rect->area();
+}
 
-#working on a square now...
-my $s1p = Point::->new('x' => 100, 'y' => 100);
-my $s2p = Point::->new('x' => 200, 'y' => 100);
-my $s3p = Point::->new('x' => 200, 'y' => 200);
-my $s4p = Point::->new('x' => 100, 'y' => 200);
+print Dumper @points;
+print Dumper scalar @points;
 
-my $square = Square::->new(('p1' => $s1p, 'p2' => $s2p, 'p3' => $s3p, 'p4' => $s4p));
+#main logic...
+given($command){
+    when('circle'){
+        if(scalar @points < 2) {
+            die "to create a circle you must supply the center and the radius\n";
+        }
 
-$square->draw();
-$square->area();
+        $f = Circle::->new(('center' => $points[0], 'point' => $points[1]));
+        $created = 1;
+    }
+    when('rectangle') {
+        if(scalar @points < 4) {
+            die "to create a circle you must supply the center and the radius\n";
+        }
 
-#working on a triangle now...
-my $t1p = Point::->new('x' => 100, 'y' => 100);
-my $t2p = Point::->new('x' => 100, 'y' => 260);
-my $t3p = Point::->new('x' => 230, 'y' => 280);
+        $f = Rectangle::->new(('p1' => $points[0], 'p2' => $points[1], 'p3' => $points[2], 'p4' => $points[3]));
+        $created = 1;
+    }
+    when('triangle') {
+        if(scalar @points < 3) {
+            die "to create a triangle you must supply 3 points\n";
+        }
 
-my $triangle = Triangle::->new(('p1' => $t1p, 'p2' => $t2p, 'p3' => $t3p));
+        $f = Triangle::->new(('p1' => $points[0], 'p2' => $points[1], 'p3' => $points[2]));
+        $created = 1;
+    }
+    when('square') {
+        if(scalar @points < 4) {
+            die "to create a square you must supply the 4 points\n";
+        }
 
-$triangle->draw();
-$triangle->area();
+        $f = Square::->new(('p1' => $points[0], 'p2' => $points[1], 'p3' => $points[2], 'p4' => $points[3]));
+        $created = 1;
+    }
+    default {
+        print "Usage:\n";
+        print "perl program.pl <command> <arguments>\n";
+        print "where command is one of rectangle, circle, triangle, square\n";
+        print "and arguments are points in the form x,y\n";
+        print "example: perl program.pl circle 40,40 70,70\n";
+    }
+}
+
+if($created){
+    $f->draw();
+}
